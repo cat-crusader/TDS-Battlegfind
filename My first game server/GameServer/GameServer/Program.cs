418a662dcb9bb91bdcd84d34352062c1b1aa6750
NIE-OleshZ <61271499+NIE-OleshZ@users.noreply.github.com>
2021-03-01 12:18:57 +0200

    first commit on server part
    my first commit on server part (can join on server)

---
+++ My first game server/GameServer/GameServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "MY_GAME_SERVER";
+            Server.Start(50, 26950);
+            Console.ReadKey();
+        }
+    }
+}
